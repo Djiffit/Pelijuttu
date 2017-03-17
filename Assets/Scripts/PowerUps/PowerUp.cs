a597946be78f99e04f4e199da3aca115888ff714
outsou <otto.hantula@hotmail.com>
2017-03-17 22:04:17 +0200

    power-up system

---
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public bool isTimed = false;
+    public float lifeTime = 5f;
+
+    protected Player player;
+
+    protected virtual void Init()
+    {
+        if (isTimed)
+            Destroy(this, lifeTime);
+    }
+}
---
+++ Assets/Scripts/PowerUps/SpeedPowerUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : PowerUp {
+    public float speedBoost = 20f;
+
+    protected override void Init()
+    {
+        base.Init();
+        player.maxSpeed += speedBoost;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            if (!other.gameObject.GetComponent<SpeedPowerUp>())
+            {
+                //Add this power-up to player and activate
+                SpeedPowerUp po = other.gameObject.AddComponent<SpeedPowerUp>();
+                po.isTimed = isTimed;
+                po.lifeTime = lifeTime;
+                po.player = other.gameObject.GetComponent<Player>();
+                po.speedBoost = speedBoost;
+                po.Init();
+
+                //Destroys the gameObject this script is attached to
+                Destroy(gameObject);
+            }
+    }
+
+    private void OnDestroy()
+    {
+        player.maxSpeed -= speedBoost;
+    }
+}
