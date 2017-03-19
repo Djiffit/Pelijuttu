5a3536904cde064ef8644d89575d4d038edf754b
outsou <otto.hantula@hotmail.com>
2017-03-19 20:10:09 +0200

    made OnTriggerEnter generic in PowerUp

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -1,17 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
     public bool isTimed = false;
     public float lifeTime = 5f;
 
-    protected Player player;
+    protected Player player = null;
 
     protected virtual void Init()
     {
         if (isTimed)
             Destroy(this, lifeTime);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // The type of this power-up
+            System.Type type = this.GetType();
+
+            if (!other.gameObject.GetComponent(type))
+            { 
+                // Add new component of type to the player object
+                PowerUp po = (PowerUp)other.gameObject.AddComponent(type);
+
+                // Copy this object's fields to the new component in player
+                System.Reflection.FieldInfo[] fields = type.GetFields();
+                foreach (System.Reflection.FieldInfo field in fields)
+                {
+                    field.SetValue(po, field.GetValue(this));
+                }
+
+                // Attach player to the power-up and initialize
+                po.player = other.gameObject.GetComponent<Player>();
+                po.Init();
+
+                // Destroy the power-up collectable object
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/PowerUps/SpeedPowerUp.cs
+++ Assets/Scripts/PowerUps/SpeedPowerUp.cs
@@ -11,26 +11,11 @@ public class SpeedPowerUp : PowerUp {
         player.maxSpeed += speedBoost;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-            if (!other.gameObject.GetComponent<SpeedPowerUp>())
-            {
-                //Add this power-up to player and activate
-                SpeedPowerUp po = other.gameObject.AddComponent<SpeedPowerUp>();
-                po.isTimed = isTimed;
-                po.lifeTime = lifeTime;
-                po.player = other.gameObject.GetComponent<Player>();
-                po.speedBoost = speedBoost;
-                po.Init();
-
-                //Destroys the gameObject this script is attached to
-                Destroy(gameObject);
-            }
-    }
-
     private void OnDestroy()
     {
-        player.maxSpeed -= speedBoost;
+        if (player != null)
+        {
+            player.maxSpeed -= speedBoost;
+        }
     }
 }
