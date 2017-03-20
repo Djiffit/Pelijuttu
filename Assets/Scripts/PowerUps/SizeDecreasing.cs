7c8b8d0d7fd8ae7952d45b172c3dbb91595f81b6
Voidbink <mikael.parvamo@gmail.com>
2017-03-20 14:31:42 +0200

    Size affecting power ups added to the project (scripts, objects, materials). Also prefabs created of the existing power ups.

---
+++ Assets/Scripts/PowerUps/SizeDecreasing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeDecreasing : PowerUp
+{
+    private Vector3 decrease = new Vector3(0.5F, 0.5F, 0.5F);
+
+    protected override void Init()
+    {
+        base.Init();
+        player.transform.localScale -= decrease;
+    }
+    private void OnDestroy()
+    {
+        if (player != null)
+        {
+            player.transform.localScale += decrease;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUps/SizeIncreasing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeIncreasing : PowerUp
+{
+    private Vector3 increase = new Vector3(1.0F,1.0F,1.0F);
+
+    protected override void Init()
+    {
+        base.Init();
+        player.transform.localScale += increase;
+    }
+    private void OnDestroy()
+    {
+        if (player != null)
+        {
+            player.transform.localScale -= increase;
+        }
+    }
+}
