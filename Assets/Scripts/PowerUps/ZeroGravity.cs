2d61c7875e9c65d7a5dfaccbc37f3a2c4d176588
Voidbink <mikael.parvamo@gmail.com>
2017-03-20 16:18:20 +0200

    ZeroGravity power-up added (object, script and the material)

---
+++ Assets/Scripts/PowerUps/ZeroGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroGravity : PowerUp
+{
+
+    protected override void Init()
+    {
+        base.Init();
+        Rigidbody playerRB = player.GetComponent<Rigidbody>();
+        playerRB.useGravity = false;
+    }
+    private void OnDestroy()
+    {
+        if (player != null)
+        {
+            Rigidbody playerRB = player.GetComponent<Rigidbody>();
+            playerRB.useGravity = true;
+        }
+    }
+}
