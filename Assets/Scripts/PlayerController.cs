c9256a9eb8513f403fc5a48138c1666f2d18192b
outsou <otto.hantula@hotmail.com>
2017-03-27 14:37:26 +0300

    pause menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,14 +12,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (Time.timeScale != 0)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        movement = Camera.main.transform.TransformDirection(movement).normalized;
-        movement.y = 0;
+            movement = Camera.main.transform.TransformDirection(movement).normalized;
+            movement.y = 0;
 
-        rigidbody.AddForce(movement * speed);
+            rigidbody.AddForce(movement * speed);
+        }
 	}
 }
