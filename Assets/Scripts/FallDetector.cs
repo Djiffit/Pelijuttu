55f7d25293a085fdb4a6df0f9480ae1651ed49c3
outsou <otto.hantula@hotmail.com>
2017-04-03 16:29:50 +0300

    tiedostojen lisäys

---
+++ Assets/Scripts/FallDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDetector : MonoBehaviour {
+
+    public float maxDistance;
+    public GameObject player;
+    
+    private float y;
+
+	void Start () {
+        // Find the lowest y coordinate
+        GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
+        y = gameObjects[0].transform.position.y;
+
+        foreach (GameObject obj in gameObjects)
+        {
+            if (obj.transform.position.y < y)
+                y = obj.transform.position.y;
+        }
+	}
+	
+	void LateUpdate () {
+		// Check if player has fallen
+        if (y - player.transform.position.y > maxDistance)
+        {
+            // Reset scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
