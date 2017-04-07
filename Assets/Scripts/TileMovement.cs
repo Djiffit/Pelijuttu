7f91400a3584f56c4be6a42d32f935c7e7654b45
Voidbink <mikael.parvamo@gmail.com>
2017-04-07 15:20:05 +0300

    Tiles added to the game with some functionality. Basic tile, broken tile (only different look) and elevating/descending tiles (moving tiles). Also powerup prefabs moved to their own folder in prefabs.

---
+++ Assets/Scripts/TileMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Transform StartPoint;
+    public Transform EndPoint;
+    public float RoundTravelTime;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = Vector3.Lerp(StartPoint.position, EndPoint.position, Mathf.Cos(Time.time / RoundTravelTime * Mathf.PI * 2) * -0.5f + 0.5f);
+        rb.MovePosition(currentPosition);
+    }
+}
