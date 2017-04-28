3ca75142a79280f7e20270a117e727f00914eb84
outsou <otto.hantula@hotmail.com>
2017-04-28 16:06:35 +0300

    powerup is now destroyed if new of the same type is picked up

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -27,27 +27,30 @@ public class PowerUp : MonoBehaviour
             // The type of this power-up
             System.Type type = this.GetType();
 
-            if (!other.gameObject.GetComponent(type))
-            { 
-                // Add new component of type to the player object
-                PowerUp po = (PowerUp)other.gameObject.AddComponent(type);
-
-                // Copy this object's fields to the new component in player
-                System.Reflection.FieldInfo[] fields = type.GetFields();
-                foreach (System.Reflection.FieldInfo field in fields)
-                {
-                    field.SetValue(po, field.GetValue(this));
-                }
-
-                // Attach player to the power-up and initialize
-                po.player = other.gameObject;
-                po.playerController = other.gameObject.GetComponent<PlayerController>();
-                po.Init();
-
-                // Attach this collectable object
-                po.collectableObject = gameObject;
-                gameObject.SetActive(false);
+            // Destroy old powerup of this type if player has it
+            if (other.gameObject.GetComponent(type))
+            {
+                Destroy(other.gameObject.GetComponent(type));
             }
+
+            // Add new component of type to the player object
+            PowerUp po = (PowerUp)other.gameObject.AddComponent(type);
+
+            // Copy this object's fields to the new component in player
+            System.Reflection.FieldInfo[] fields = type.GetFields();
+            foreach (System.Reflection.FieldInfo field in fields)
+            {
+                field.SetValue(po, field.GetValue(this));
+            }
+
+            // Attach player to the power-up and initialize
+            po.player = other.gameObject;
+            po.playerController = other.gameObject.GetComponent<PlayerController>();
+            po.Init();
+
+            // Attach this collectable object
+            po.collectableObject = gameObject;
+            gameObject.SetActive(false);
         }
     }
 
