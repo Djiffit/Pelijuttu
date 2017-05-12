765718afb145b46a1d7ce35e8955d1221d8d371d
Voidbink <mikael.parvamo@gmail.com>
2017-05-12 14:56:28 +0300

    Gems now have sounds when picked and powerups have custom sounds when picked

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,10 +2,13 @@
 
 public class Collectable : MonoBehaviour
 {
+    public AudioClip activationSound;
+
     protected virtual void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(activationSound, transform.position);
             Destroy(gameObject);
         }
     }
